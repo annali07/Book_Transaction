76e4629431bd4c79356bba749a085629cf90eaac
Yuchen Hua <yuchen.hua03@icloud.com>
2024-06-29 22:56:02 -0400

    create list

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -1,5 +1,22 @@
 package entity;
 
+import java.util.Date;
+
 public class Book {
+    private String bookName;
+    private int bookID;
+    private String isbn;
+    private boolean damageStatus;
+    private boolean availability;
+    private String rentalOrPurchase;
+    private double bookPrice;
+    private double rentalPrice;
+    private Date rentalStartDate;
+    private Date rentalEndDate;
+
+
 
 }
+
+
+
