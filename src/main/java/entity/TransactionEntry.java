9e233c836ef192605e0aedd71157887d688ef823
Kenny Zhang <zhenyi.zhang@mail.utoronto.ca>
2024-07-04 13:08:47 -0400

    I add a new function to RentalEntry by calculating the max of charge and maxcharge

--- src/main/java/entity/TransactionEntry.java
+++ src/main/java/entity/TransactionEntry.java
@@ -1,4 +1,60 @@
 package entity;
+import  java.util.Date;
 
 public class TransactionEntry {
+    private int transactionId;
+    private int bookId;
+    private String bookName;
+    private double soldPrice;
+    private Date date;
+
+    public TransactionEntry(int transactionId, int bookId, String bookName, double soldPrice, Date date) {
+        this.transactionId = transactionId;
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.soldPrice = soldPrice;
+        this.date = date;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public double getSoldPrice() {
+        return soldPrice;
+    }
+
+    public void setSoldPrice(int soldPrice) {
+        this.soldPrice = soldPrice;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
 }
