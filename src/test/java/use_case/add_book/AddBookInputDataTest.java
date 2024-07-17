a9ec8d3f14f0ba6064d9196f2a4710d96c627d64
anna <annaxzl05@gmail.com>
2024-07-17 10:16:13 -0400

    unit tests to be implemented

---
+++ src/test/java/use_case/add_book/AddBookInputDataTest.java
@@ -0,0 +1,26 @@
+package use_case.add_book;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddBookInputDataTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getIsbn() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+}
