a562354ea48f8b062eb7000a90627374a2438886
Kenny Zhang <113133676+Kenny2003-08-28@users.noreply.github.com>
2024-07-19 13:09:47 -0400

    Update ReturnOrBorrowPresenter.java

--- src/main/java/interface_adapter/returnorborrow/ReturnOrBorrowPresenter.java
+++ src/main/java/interface_adapter/returnorborrow/ReturnOrBorrowPresenter.java
@@ -72,6 +72,7 @@ public class ReturnOrBorrowPresenter implements RobOutputBoundary {
         viewManagerModel.firePropertyChanged();
         System.out.println("Switched from Return or Borrow View to rent Menu");
         
-       
+
+        
     }
 }
